ee28bcc7640a0cdcd97944a7fcc07689af084036
Nguyễn Quang Hùng <101113119+Appledog2k@users.noreply.github.com>
2022-03-10 20:54:51 +0700

    Add files via upload

---
+++ Loops.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//! FOR LOOP
+/* 
+for ( Initialization; Condition; Update ) {
+  ...Commands...
+}
+*/
+
+// printf Hello, World !
+ for(int i=1 ; i<=10 ; i++) {
+     Console.WriteLine($"Giá trị của i là: {i}");
+     Console.WriteLine("Hello, World!");
+}
+
+// printf tất cả các số chẵn từ 0-a
+int a;
+Console.Write("Nhập vào số a:");
+a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Số a vừa nhập là: {a}");
+for(int i=0; i<=a ;i++) {
+    if(i % 2 != 0) continue;
+    Console.WriteLine($"{i} là số chẵn");
+}
+
