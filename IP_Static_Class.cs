77b4f03631227dcc8b001bcb6406c8ba2eb9c2e0
Nguyễn Quang Hùng <101113119+Appledog2k@users.noreply.github.com>
2022-03-12 22:33:48 +0700

    12/03 Import_Static_Class

---
+++ IP_Static_Class.cs
@@ -0,0 +1,16 @@
+namespace staticOpp {
+    static class Calculator {
+        public static int Sum(int a, int b) {
+                return a+b;
+        }
+        public static int Sub(int a, int b) {
+                return a-b;
+        }
+        public static int Mul(int a, int b) {
+                return a*b;
+        }
+        public static float Div(int a, int b) {
+                return (float)a/b;
+        }
+    }
+}
---
+++ IP_Static_Main.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.AccessControl;
+using Microsoft.VisualBasic.CompilerServices;
+// See https://aka.ms/new-console-template for more information
+
+
+//! thực thiện import static class 
+using static staticOpp.Calculator;
+using static System.Console;
+
+Console.WriteLine("Hello, World!");
+int a=120, b=500;
+
+WriteLine("a+b="+Sum(a,b));
