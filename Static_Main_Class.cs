8cc79b25b8045bcccd1d00afac789d1a388f5843
Nguyễn Quang Hùng <101113119+Appledog2k@users.noreply.github.com>
2022-03-12 22:27:39 +0700

    12/03 Static_Class

---
+++ Static_Main_Class.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Emit;
+using System;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//! Lớp static không thể tạo biến
+//! Lớp static không thể tạo đối tượng
+// Lớp static là lớp tiện ích cung cấp tính năng cho lớp khác, đối tượng khác mà không cần dùng đến đối tượng lớp tiện ích đó mà vẫn có thể sử dụng
+
+int a=120, b=500;
+
+System.Console.WriteLine("a+b="+ staticOpp.Calculator.Sum(a,b));
