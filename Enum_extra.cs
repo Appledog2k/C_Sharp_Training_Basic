34687bafaf0711a7e2713c6e852fe6dc36299ea9
Nguyễn Quang Hùng <101113119+Appledog2k@users.noreply.github.com>
2022-03-13 23:33:06 +0700

    Lecture 4: 14/03/2022 Enum

---
+++ Enum_extra.cs
@@ -0,0 +1,17 @@
+//! list type === enum
+/* 0-kém
+   1-Trung bình
+   2-Khá
+   3-Giỏi
+*/
+namespace Enum
+{
+    enum Learning_ability
+    {
+        Kem = 100,         //0
+        TrungBinh = 200,  //1
+        Kha = 300,          //2
+        Gioi = 400        //3
+
+    }
+}
---
+++ Enum_main.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//! Enum === TYPE LIST
+
+// initialization Enum
+Enum.Learning_ability learning_Ability;
+
+
+learning_Ability = Enum.Learning_ability.Kha;
+
+// Convert enum to number
+
+int number = (int)learning_Ability;
+System.Console.WriteLine(number);
+
+// Convert number to enum
+
+learning_Ability = (Enum.Learning_ability)(300);
+System.Console.WriteLine(learning_Ability);
+
+
+switch (learning_Ability)
+{
+    case Enum.Learning_ability.Kem:
+        System.Console.WriteLine("Học lực kém");
+        break;
+    case Enum.Learning_ability.TrungBinh:
+        System.Console.WriteLine("Học lực trung bình");
+        break;
+    case Enum.Learning_ability.Kha:
+        System.Console.WriteLine("Học lực khá");
+        break;
+    case Enum.Learning_ability.Gioi:
+        System.Console.WriteLine("Học lực giỏi");
+        break;
+    default:
+        System.Console.WriteLine("Không có đối tượng này");
+        break;
+}
