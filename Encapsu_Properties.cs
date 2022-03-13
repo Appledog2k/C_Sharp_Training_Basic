0207d8a592886b62c547ddee2779e6b6ff937abf
Nguyễn Quang Hùng <101113119+Appledog2k@users.noreply.github.com>
2022-03-13 10:55:38 +0700

    lecture3: 12/03 Encapsu_Properties_Get-Set

---
+++ Encapsu_Properties.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* là sự kết hợp bằng cách định nghĩa cặp get/set trong bản thân môt property
+   không sử dụng cặp ngoặc ()
+   có kiểu trả về là kiểu của trường mà nó đóng gói
+
+*/
+
+encapsulation_get_set.Student Hùng = new encapsulation_get_set.Student(2018000, "Hùng", "hung.nq182556@sis.hust.edu.vn", 20);
+System.Console.WriteLine("Tuổi:" + Hùng.Age);
+Hùng.Age++;
+System.Console.WriteLine("Tuổi:" + Hùng.Age);
+System.Console.WriteLine("Tên:" + Hùng.FirstName);
+Hùng.ID = 20182556;
+System.Console.WriteLine("Mã sinh viên:" + Hùng.ID);
---
+++ Encapsu_pro_class.cs
@@ -0,0 +1,83 @@
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+namespace encapsulation_get_set
+{
+    class Student
+    {
+        private int id;
+        private string firstName;
+        private string email;
+        private int age;
+        public Student(int id, string firstName, string email, int age)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.email = email;
+            if (age < 0 || age > 200)
+            {
+                System.Console.WriteLine("Tuổi nhập không hợp lệ. Vui lòng nhập lại.");
+                this.age = default;
+            }
+            else
+            {
+                this.age = age;
+            }
+        }
+
+        // property --- get +set
+        public int ID
+        {
+            get { return id; }
+            set
+            {
+                if (value > 0 && value <= 30000000)
+                {
+                    this.id = value;
+                }
+                else
+                {
+                    System.Console.WriteLine("Vui lòng nhập lại mã sinh viên");
+                }
+            }
+        }
+
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0 || value > 200)
+                {
+                    System.Console.WriteLine("Tuổi nhập không hợp lệ.");
+                }
+                else
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if (value.Length > 0 && value.Length <= 15)
+                {
+                    this.firstName = value;
+                }
+                else
+                {
+                    System.Console.WriteLine("Tên không hợp lệ.");
+                }
+
+            }
+
+
+        }
+    }
+}
