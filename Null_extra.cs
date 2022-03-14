d1bbb3da5864c80f33ea21eb8a74c6d729cd751c
Nguyễn Quang Hùng <101113119+Appledog2k@users.noreply.github.com>
2022-03-14 21:46:22 +0700

    Lecture 4: 14/03  Null_Nullable

---
+++ Null_extra.cs
@@ -0,0 +1,8 @@
+using System;
+namespace Null {
+    class Null {
+        public void Hi(){
+            System.Console.WriteLine("Hello, I'm Hung");
+        }
+    }
+}
---
+++ Null_main.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Null.Null a = new Null.Null();
+
+//if a!= null access method Hi
+a?.Hi();
+
+
+// nullAble
+
+int? age;
+age =null;
+
+age =10;
+if( age.HasValue ) {
+    int _age = age.Value;
+    System.Console.WriteLine(_age);
+}
+
+
