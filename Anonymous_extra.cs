c904ec0c0c2cfefbffca52b1ecafe7b870056a98
Nguyễn Quang Hùng <101113119+Appledog2k@users.noreply.github.com>
2022-03-14 16:59:49 +0700

    lecture 4 : 14/03 : Anonymuos

---
+++ Anonymous_extra.cs
@@ -0,0 +1,11 @@
+namespace Anomynuos{
+    class student {
+        public string Name { get; set; }
+        public int YearOfBirth { get; set; }
+        public string PlaceOfBirth { get; set; }
+
+    }
+
+
+
+}
---
+++ Anonymous_main.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.AccessControl;
+// See https://aka.ms/new-console-template for more information
+
+
+// Type data  anonymous 
+// object property read only
+// new {nameAttribute1 =value, nameAttribute2 =value}
+using System.Linq;
+
+var product = new
+{
+    name = "IPhone",
+    Price = 500,
+    YearOfManufacture = 2018,
+};
+System.Console.WriteLine(product.name);
+
+
+
+
+List<Anomynuos.student> Students = new List<Anomynuos.student>() {
+    new Anomynuos.student() {
+    Name = "Name1",
+    YearOfBirth =   2000,
+    PlaceOfBirth ="BG"
+
+    },
+    new Anomynuos.student() {
+    Name = "Name2",
+    YearOfBirth =   2002,
+    PlaceOfBirth ="BN"
+
+    },
+    new Anomynuos.student() {
+    Name = "Name3",
+    YearOfBirth =   2003,
+    PlaceOfBirth ="HN"
+
+    },
+    new Anomynuos.student() {
+    Name = "Name4",
+    YearOfBirth =   20004,
+    PlaceOfBirth ="HP"
+
+    }
+};
+
+// using Linq
+var results = from Student in Students
+              where Student.Name.Contains("a")
+              select new {
+                 Name = Student.Name,
+                 PlaceOfBirth = Student.PlaceOfBirth
+              };
+
+        foreach (var Student in results)
+        {
+              System.Console.WriteLine(Student.Name+ " - "+Student.PlaceOfBirth);
+        }
+              
